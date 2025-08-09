c860a10a2abdce136180f4bcc9e2bb8aa1ff5e69
Gayathri G <gayathrigaya3ece@gmail.com>
2025-08-09 20:38:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HeroSection from './components/HeroSection';
 import Skills from './components/Skills';
 import ReactProjects from './components/ReactProjects';
 import About from './components/About';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <About /> 
       <Skills/>
       <ReactProjects/>
+      <Footer/>
       
     </>
   );
