bac6664e05a481324d1210cc57b6adcf3bb86373
GAYATHRI G <gayathrigaya3ece@gmail.com>
2025-08-09 20:58:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,16 @@ const Footer = () => {
   return (
     <>
       <section id="contact">
-        
+         <div id='footer' className="footer  w-full bg-gray-800 shadow-lg">
+        <div className="container mx-auto px-4 py-4 text-center">
+            <p className="text-gray-400">© 2025 Gayathri G. All rights reserved. </p>
+            <div className="social-links mt-4">
+            <a href="https://www.linkedin.com/in/gayathri1006/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mx-2">LinkedIn</a>
+            <a href="https://github.com/GAYATHRI1006" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mx-2">GitHub</a>
+            <a href="https://www.instagram.com/_._.unique_one._._/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline mx-2">Instagram</a>
+            </div>
+            </div>
+        </div>
       </section>
     </>
   )
